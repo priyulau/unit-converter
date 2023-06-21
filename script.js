496993ef91e005fe89e51808636ef395de39d01b
Priscilla Lau <pyu@wellesley.edu>
2023-06-21 10:31:58 -0400

    updated js with left and right multipliers

--- script.js
+++ script.js
@@ -2,27 +2,30 @@ const conversionType = document.querySelector("#conversion");
 const converterInputs = document.querySelectorAll(".converter-input");
 
 const length = {
-  units: [`Meter`, `Foot`, `Inch`],
-  multiplier: 0.6214,
-  formula: `Formula: for an approximate result, multiply the length value by 0.621`,
+  units: [`Meters`, `Feet`, `Inches`],
+  // unitsLeft: [1, 2, 3],
+  // unitsRight: [3,2,1],
+  formula: `Formula: for an approximate result, multiply the length value by __ to convert to base unit meters and then multiply by ___`,
 };
 
 const mass = {
   units: [`Kilograms`, `Grams`, `Pounds`, `Ounces`],
-  multiplier: 2.35215,
-  formula: `Formula: for an approximate result, multiply the fuel economy value by 2.35`,
+  // unitsLeft: [1, 2, 3, 4],
+  // unitsRight: [4, 3, 2, 1],
+  formula: `Formula: for an approximate result, multiply the mass value by __ to convert to base unit grams and then multiply by ___`,
 };
 
 const volume = {
   units: [`Liters`, `Milliliters`, `Gallons`],
-  multiplier: 0.001,
-  formula: `Formula: divide the digital storage value by 1000`,
+  // unitsLeft: [1, 2, 3],
+  // unitsRight: [3, 2, 1],
+  formula: `Formula: for an approximate result, multiply the volume value by __ to convert to base unit liters and then multiply by ___`,
 };
 
 //event listener for conversion type dropdown
 conversionType.addEventListener("change", (event) => {
   updateUnits(event.target.value);
-  updateMultiplier(inputValues.conversion);
+  // updateMultiplier(inputValues.conversion);
   updateFormula(event.target.value);
 });
 
@@ -52,11 +55,17 @@ converterInputs.forEach((convertInputs) => {
       fromValue: document.querySelector("#from-value").value,
       //grab the toUnit value at that moment in time
       toUnit: document.querySelector("#to-unit").value,
+      //grab the fromValue value at that moment in time
+      toValue: document.querySelector("#to-value").value,
       //grab the category value at that moment in time
       conversion: document.querySelector("#conversion").value,
     };
-    getInputValues(inputValues);
-    updateMultiplier(inputValues.conversion);
+    // getInputValues(inputValues);
+    calcNewValue(
+      updateLeftMultiplier(inputValues.fromUnit),
+      updateRightMultiplier(inputValues.toUnit)
+    );
+    // updateMultiplier(inputValues.conversion);
   });
 });
 
@@ -65,21 +74,81 @@ function getInputValues(inputValuesObject = {}) {
   console.log(inputValuesObject);
 }
 
-function updateMultiplier(type) {
-  if (type === `length`) {
-    calcNewValue(length.multiplier);
+//function that generates a factor based on the from-unit to the base unit
+//(e.g.grams)
+function updateLeftMultiplier(unit) {
+  if (unit === `Kilograms`) {
+    return 1000; // when base unit = 1...
   }
-  if (type === `mass`) {
-    calcNewValue(mass.multiplier);
+  if (unit === `Grams`) {
+    return 1;
   }
-  if (type === `volume`) {
-    calcNewValue(volume.multiplier);
+  if (unit === `Pounds`) {
+    return 453.5924;
+  }
+  if (unit === `Ounces`) {
+    return 28.34952;
+  }
+  if (unit === `Meters`) {
+    return 1;
+  }
+  if (unit === `Feet`) {
+    return 3.28084;
+  }
+  if (unit === `Inches`) {
+    return 39.37008;
+  }
+  if (unit === `Liters`) {
+    return 1;
+  }
+  if (unit === `Milliliters`) {
+    return 1000;
+  }
+  if (unit === `Gallons`) {
+    return 0.2641729;
   }
 }
 
-function calcNewValue(multiplierFactor) {
+//function that generates a factor based on the to-unit from the base unit
+//(e.g.grams)
+function updateRightMultiplier(unit) {
+  if (unit === `Ounces`) {
+    return 0.03527396;
+  }
+  if (unit === `Pounds`) {
+    return 0.002204623;
+  }
+  if (unit === `Grams`) {
+    return 1;
+  }
+  if (unit === `Kilograms`) {
+    return 0.001;
+  }
+  if (unit === `Meters`) {
+    return 1;
+  }
+  if (unit === `Feet`) {
+    return 3.28084;
+  }
+  if (unit === `Inches`) {
+    return 0.3048;
+  }
+  if (unit === `Liters`) {
+    return 1;
+  }
+  if (unit === `Milliliters`) {
+    return 0.001;
+  }
+  if (unit === `Gallons`) {
+    return 3.7854;
+  }
+}
+
+function calcNewValue(leftMultiplier, rightMultiplier) {
   document.querySelector("#to-value").value =
-    document.querySelector("#from-value").value * multiplierFactor;
+    document.querySelector("#from-value").value *
+    leftMultiplier *
+    rightMultiplier;
 }
 
 //function that updates the formula box based on type
